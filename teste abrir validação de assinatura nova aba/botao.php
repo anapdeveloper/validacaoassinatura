37a2f773ff6f785ae3ff00a64b60aac20fa43e3f
anapdeveloper <anapgrafic@yahoo.com>
2025-02-06 00:37:36 -0300

    Add files via upload

---
+++ teste abrir validação de assinatura nova aba/botao.php
@@ -0,0 +1,9 @@
+<?php
+// Seu código PHP existente aqui...
+?>
+
+<button onclick="window.open('https://validar.iti.gov.br/', '_blank')">Validar Receita</button>
+
+<?php
+// Seu código PHP continua aqui...
+?>
